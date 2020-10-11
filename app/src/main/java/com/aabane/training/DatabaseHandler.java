6277806c1cc3f9ab9053251554d88ff9bd0a1755
unknown <abderrahimdeaabane@gmail.com>
2020-10-11 23:32:40 +0100

    added a table for training exercises

--- app/src/main/java/com/aabane/training/DatabaseHandler.java
+++ app/src/main/java/com/aabane/training/DatabaseHandler.java
@@ -14,7 +14,22 @@ public class DatabaseHandler extends SQLiteOpenHelper {
             Training_id + " INTEGER PRIMARY KEY AUTOINCREMENT," +
             Training_Name + " VARCHAR(50), " +
             Training_Image + " BLOB);";
+    private static String Exercise_Table = "exercise";
+    private static String Exercise_id = "exercise_id";
+    private static String Exercise_Name = "exercise_name";
+    private static String Exercise_Weight = "exercise_weight";
+    private static String Exercise_reps = "exercise_reps";
+    private static String Exercise_Image = "exercise_image";
+    private static String Create_Table_Exercise_Query = "CREATE TABLE " + Exercise_Table + " (" +
+            Exercise_id + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+            Exercise_Name + " VARCHAR(50)," +
+            Exercise_Image + " BLOB," +
+            Exercise_Weight + " NUMBER(9),"+
+            Training_id + " INTEGER,"+
+            Exercise_reps + " INTEGER," +
+            "FOREIGN KEY ("+Training_id+") REFERENCES "+Training_Table+"("+Training_id+") );";
     private static String DELETE_Training_Query = "DROP TABLE " + Training_Table +";";
+    private static String DELETE_Exercise_Query = "DROP TABLE " + Exercise_Table +";";
 
     public  DatabaseHandler(Context context, String name, SQLiteDatabase.CursorFactory factory,int version){
         super(context,name,factory,version);
@@ -22,6 +37,7 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         sqLiteDatabase.execSQL(Create_Table_Training_Query);
+        sqLiteDatabase.execSQL(Create_Table_Exercise_Query);
     }
 
     @Override
@@ -29,5 +45,6 @@ public class DatabaseHandler extends SQLiteOpenHelper {
 
         sqLiteDatabase.execSQL(DELETE_Training_Query);
         sqLiteDatabase.execSQL(Create_Table_Training_Query);
+        sqLiteDatabase.execSQL(Create_Table_Exercise_Query);
     }
 }
